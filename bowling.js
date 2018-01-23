8eeaf1cfded14777c561a19b7231774dcb85472d
krishna_dixit <rkdixit96@gmail.com>
2018-01-23 11:42:21 +0530

    Basic functionality testing with no consecutive strikes, strike/spare at last turn

--- bowling.js
+++ bowling.js
@@ -1,7 +1,56 @@
 function getScore(arr){
     let score = 0;
+    let frame;
+    let turn;
+    let previous;
+    let frameSums = [];
+    for(turn = 0; turn < 10; turn++){
+        console.log(previous)
+        console.log(turn);
+        frameSums[turn] = 0;
+        let i;
+        
+        for(i = turn*2; i <= ((turn*2)+ 1) ; i++){
+            console.log("   ",i);
+            frameSums[turn] += arr[i];
+        }
+
+        console.log("value of i",i)
+
+
+        //Bonus score adding when strike or spare
+        if(previous === "strike"){
+            frameSums[turn-1] += frameSums[turn]
+        }
+        else if(previous === "spare"){
+            frameSums[turn-1] += arr[i-2]
+        }
+
+        //Checking if current is strike or spare
+        previous = ""
+        if(frameSums[turn]===10){
+            if(arr[i-1]!=0){
+                previous = "spare"
+            }
+            else{
+                previous = "strike"
+            }
+        }
+        console.log("      ", frameSums[turn])
+
+    }
+
+    console.log(frameSums);
+
+    for(i=0;i<frameSums.length;i++){
+        score += frameSums[i]
+    }
+
+    
 
     return score;
 }
+
+
 
 module.exports = getScore;
--- bowling.test.js
+++ bowling.test.js
@@ -1,5 +1,11 @@
 const getScore = require("./bowling");
 
 test("Returns a number", () => {
-    expect(typeof getScore([])).toBe('number');
+    let array1 = [10,0,7,2,7,3,1,1,0,0,5,4,10,0,1,8,5,5,2,1];
+    expect(typeof getScore(array1)).toBe('number');
+})
+
+test("Basic functinality with no consecutive strikes, strike/spare at last turn", () => {
+    let array1 = [10,0,7,2,7,3,1,1,0,0,5,4,10,0,1,8,5,5,2,1];
+    expect(getScore(array1)).toBe(93)
 })
